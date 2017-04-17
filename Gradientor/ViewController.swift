e330834b81fb0f89f16a3e5a8d4dc013b4134dae
tnantoka <tnantoka@bornneet.com>
2017-04-17 22:49:00 +0900

    Create project with RxSwift pod

---
+++ Gradientor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gradientor
+//
+//  Created by Tatsuya Tobioka on 2017/04/17.
+//  Copyright © 2017 tnantoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+target 'Gradientor' do
+  use_frameworks!
+
+  pod 'RxSwift'
+
+  target 'GradientorTests' do
+    inherit! :search_paths
+  end
+end
