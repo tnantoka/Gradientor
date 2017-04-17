b9a04133336269bad3e890809c63dcce7a05c18d
tnantoka <tnantoka@bornneet.com>
2017-04-17 22:58:56 +0900

    Delete Main.storyboard

--- Gradientor/AppDelegate.swift
+++ Gradientor/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let homeViewController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: homeViewController);
+        self.window?.rootViewController = navigationController
+
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
